5e345a724c898e73e1bf89aca33704bb449a1724
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-14 10:57:12 -0600

    created home route testing back end data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
+import React, {useState, useEffect} from "react"
+import RoomCard from "./RoomCard"
+import {Route, Routes} from "react-router-dom"
 
 function App() {
+  const [rooms, setRooms] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/rooms")
+      .then((r) => r.json())
+      .then((data) => setRooms(data));
+
+  }, []);
+
+  console.log(rooms)
+
   return (
     <div >
- 
+      <Routes>
+        <Route path="/" element={<RoomCard/>}/>
+
+
+      </Routes>
     </div>
   );
 }
