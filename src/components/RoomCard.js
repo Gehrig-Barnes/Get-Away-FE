8311714d22d525bc94978651f42c440609ce5a2f
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-14 11:22:08 -0600

    added data to card

--- src/components/RoomCard.js
+++ src/components/RoomCard.js
@@ -1,8 +1,14 @@
 import React from "react"
-function RoomCard(){
+function RoomCard({address, living_type, image, title, price, rating}){
     return (
         <div>
-            <h3>hello</h3>
+            <h2>{title}</h2>
+            <img src={image}/>
+            <h2>Living Type: {living_type}</h2>
+            <h3>{address}</h3>
+            <h3>Rating: {rating}/10</h3>
+            <h3>${price}/night</h3>
+            
         </div>
     )
 }
