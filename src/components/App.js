f1998f4427501f95821c0b2df244bb05c30eb21d
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-15 16:12:59 -0600

    added calender

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,27 @@ import RoomList from "./RoomList";
 import {Route, Routes} from "react-router-dom";
 import Navigation from './Navigation';
 import RoomDetails from './RoomDetails'
+import Login from './Login'
 import "../App.css";
 
 //create a route for each home. 
 //when we click on a home, it should bring us to a page where we see detail and we can checkout(reserve room)
-
+// jlee@aol.com
 function App() {
   const [rooms, setRooms] = useState([])
+  const [host, setHost] = useState([])
+  const [email, setEmail] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/host/${email}`)
+      .then((r) => r.json())
+      .then((data) => setHost(data));
+
+  }, []);
+
+  console.log(host)
+
+  console.log(email)
 
 
   useEffect(() => {
@@ -31,6 +45,8 @@ console.log(rooms);
       <Routes>
       <Route exact path='/rooms/:id' element={<RoomDetails/>}/>
       <Route exact path="/rooms" element={<RoomList rooms={rooms}/>}/>
+      <Route exact path='/login' element={<Login setEmail={setEmail}/>}/>
+      <Route exact path='/host'/>
       
 
         
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Login () {
+function Login ({setEmail}) {
+    
+    function manageEmail(e){
+        const value = e.target.value
+        setEmail(value)
+    }
+
+    
+
     return (
-        <h1>this is the login</h1>
+        <div>
+            <h1>this is the login</h1>
+            <form >
+                <label htmlFor="login" value="Email">Email:   </label><br/>
+                <input type="text" name="login" onChange={manageEmail}  />
+                <input type="submit" value="Login"/>
+            </form>
+        </div>
+        
+        
     )
 }
 
