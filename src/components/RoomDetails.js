2d9985231f641f032de6121a76c3a64cac7d453a
Jennifer Tchai <jentchai@gmail.com>
2022-02-15 14:16:19 -0500

    setting up room details page

--- src/components/RoomDetails.js
+++ src/components/RoomDetails.js
@@ -1,8 +1,23 @@
 import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Calendar } from 'react-date-range';
+import { DateRangePicker } from 'react-date-range';
+import { addDays } from 'date-fns';
+
 function RoomDetails () {
     const[room, setRoom] = useState(true)
     const {id} = useParams();
+    const [state, setState] = useState([
+      {
+        startDate: new Date(),
+        endDate: addDays(new Date(), 7),
+        key: 'selection'
+      }
+    ]);
 
     useEffect(() => {
         fetch(`http://localhost:9292/rooms/${id}`)
@@ -14,10 +29,29 @@ function RoomDetails () {
       }, [id]);
 
     return (
-        <div>
-            <h2>hello</h2>
+      <Container>
+        <Row>
+          <Col>
             <h2>{room.title}</h2>
-        </div>
+            <img src={room.image} />
+            <h2>{room.description}</h2>
+            <h2>Rating: {room.rating}/10</h2>
+            <h2>Comment: {room.comment}</h2>
+          </Col>
+          <Col className="rightcolumn">
+            <h2>${room.price}/night</h2>
+            <h2>Rating: {room.rating}/10</h2>
+            <DateRangePicker
+              onChange={item => setState([item.selection])}
+              showSelectionPreview={true}
+              moveRangeOnFirstSelection={false}
+              months={2}
+              ranges={state}
+              direction="horizontal"
+            />;
+          </Col>
+        </Row>
+      </Container>
     )
 }
 
